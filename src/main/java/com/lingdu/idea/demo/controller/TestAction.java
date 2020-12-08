9291358f5ac8b8df8927403ba9f5a880df22d1f1
lingdu <xxxx@qq.com>
2020-12-08 22:01:01 +0800

    m

--- src/main/java/com/lingdu/idea/demo/controller/TestAction.java
+++ src/main/java/com/lingdu/idea/demo/controller/TestAction.java
@@ -1,5 +1,6 @@
 package com.lingdu.idea.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,9 @@ import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class TestAction {
+
+    @Value("${test:默认值}")
+    private String test;
 
     // 测试地址： http://192.168.116.115:8888/hello?name=lingdu
     @GetMapping("/hello")
@@ -19,10 +23,10 @@ public class TestAction {
         String status = request.getParameter("status");
 
 
-        return "Hello World!";
+        return "Hello World! " + test;
     }
 
-    private  void test(){
+    private void test() {
         System.out.println("test");
     }
 
